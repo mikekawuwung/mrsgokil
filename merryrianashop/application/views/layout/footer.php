8c57372e143dd3ea12de15995ddb7514e44e0594
mikekawuwung <kawuwungmichael@gmail.com>
2020-12-02 14:59:56 +0700

    create layout

---
+++ merryrianashop/application/views/layout/footer.php
@@ -0,0 +1,83 @@
+				<footer class="footer" >
+					<div class="container clearfix">
+            <div class="row p-3" style="background:#414042">
+              <div class="col-lg-12">
+              <span class="text-white d-block text-center text-sm-left d-sm-inline-block" style="font-size: 20px;">
+              FOLLOW US : 
+              <i class="fab fa-facebook pl-4"></i>
+              <i class="fab fa-twitter pl-4"></i>
+              <i class="fab fa-instagram pl-4"></i>
+              <i class="fab fa-youtube pl-4"></i>
+            </span>
+			
+              </div>
+            </div>
+            
+            <div class="row pt-3">
+              <div class="col-lg-2">
+                <p><b>INFORMASI</b></p>
+                <p>
+                  TENTANG KAMI
+                  <br>
+                  HUBUNGAN KAMI
+                  <br>
+                  STORE
+                </p>
+              </div>
+              <div class="col-lg-3">
+                <small>Didirikan pada tahun 2007, Insight Unlimited adalah brand yang bergerak di area Distribusi, Retail dan Publikasi Musik Kristiani di Indonesia dan Asia Tenggara. Produk kami telah didistribusikan ke berbagai belahan dunia.</small>
+                <br>
+                <small>Di tahun 2015, kami secara aktif mengelola Flagship Store yang kami miliki di Jakarta dan mendistribusikan produk kami yang terdiri dari berbagai jenis produk mulai dari musik, buku, fashion dan aksesoris ke toko buku dan toko musik di Indonesia, bahkan ke Asia Tenggara.</small>
+              </div>
+              <div class="col-lg-3">
+              <p><b>LAYANAN</b></p>
+              <p>
+                  CARA PEMBELIAN
+                  <br>
+                  PERTANYAAN UMUM
+                  <br>
+                  SYARAT & KETENTUAN
+                  <br>
+                  PENCARIAN LANJUTAN
+              </p>
+              </div>
+              <div class="col-lg-4">
+              <p><b style="color:#ae0001">LIHAT STATUS ORDER<br>
+              KONFIRMASI PEMBAYARAN</b>
+              <br>
+              <b>BE THE FIRST TO KNOW</b></p><br>
+              
+              </div>
+            </div>
+				
+					</div>
+				</footer>
+				<!-- partial -->
+
+			</div>
+			<!-- main-panel ends -->
+		</div>
+		<!-- page-body-wrapper ends -->
+	</div>
+	<!-- container-scroller -->
+	<!-- plugins:js -->
+	<script src="<?php echo base_url(); ?>assets/src/assets/vendors/js/vendor.bundle.base.js"></script>
+	<script src="<?php echo base_url(); ?>assets/src/assets/vendors/js/vendor.bundle.addons.js"></script>
+	<!-- endinject -->
+	<!-- Plugin js for this page-->
+	<!-- End plugin js for this page-->
+	<!-- inject:js -->
+	<script src="<?php echo base_url(); ?>assets/src/assets/js/shared/off-canvas.js"></script>
+	<script src="<?php echo base_url(); ?>assets/src/assets/js/shared/hoverable-collapse.js"></script>
+	<script src="<?php echo base_url(); ?>assets/src/assets/js/shared/misc.js"></script>
+	<script src="<?php echo base_url(); ?>assets/src/assets/js/shared/settings.js"></script>
+	<script src="<?php echo base_url(); ?>assets/src/assets/js/shared/todolist.js"></script>
+	<!-- endinject -->
+	<!-- Custom js for this page-->
+	<script src="<?php echo base_url(); ?>assets/src/assets/js/shared/widgets.js"></script>
+	<script src="<?php echo base_url(); ?>assets/src/assets/js/demo_2/dashboard.js"></script>
+	<script src="<?php echo base_url(); ?>assets/src/assets/js/demo_2/script.js"></script>
+	<!-- End custom js for this page-->
+</body>
+
+</html>
---
+++ merryrianashop/application/views/layout/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Merry Riana Shop</title>
+	<!-- plugins:css -->
+	<link rel="stylesheet"
+		href="<?php echo base_url(); ?>assets/src/assets/vendors/iconfonts/mdi/css/materialdesignicons.min.css">
+	<link rel="stylesheet"
+		href="<?php echo base_url(); ?>assets/src/assets/vendors/iconfonts/puse-icons-feather/feather.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/src/assets/vendors/css/vendor.bundle.base.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/src/assets/vendors/css/vendor.bundle.addons.css">
+	<!-- endinject -->
+	<!-- plugin css for this page -->
+	<!-- End plugin css for this page -->
+	<!-- inject:css -->
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/src/assets/css/shared/style.css">
+	<!-- endinject -->
+	<!-- Layout styles -->
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/src/assets/css/demo_2/mrshop.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/src/assets/css/demo_2/style.css">
+	<!-- End Layout styles -->
+  <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/Logo_MRS_PNG-03.png" />
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+</head>
